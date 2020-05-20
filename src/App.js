4c322c9771d1bbd1c24b16f4d053d66b822cfed0
avnendrabhadauria <50137078+avnendrabhadauria@users.noreply.github.com>
2020-05-20 17:57:37 +0530

    Revert "Update App.js"
    This reverts commit 905c10f672399fe84ce478340fa8dcc02051c6a7.

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ export default class App extends Component {
 
         return (<div className='App' >
             <div className='title'>
-                <div>COVID-19</div>
+                <div>Coronaa</div>
             </div>
             <div className='header '>
                 <div style={{ height: "99%" }}>
